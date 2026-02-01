ee1e1b21f7f18e18efd68922fa502f1c7a7b1185
Justin Doctolero <jdoctolero1@gmail.com>
2026-01-31 22:37:46 -0500

    task: parameterize the deploy-governance.ps1 file.

--- scripts/deploy-governance.ps1
+++ scripts/deploy-governance.ps1
@@ -1,4 +1,8 @@
+param (
+  [string]$Region = "centralus"
+)
+
 Write-Host "[INFO] Deploying Policies to Subscription"
 az deployment sub create `
-  --location centralus `
+  --location $Region `
   --template-file ./orchestration/main.bicep
